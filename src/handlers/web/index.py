e0f7f152b8fa94ddb7cc2e70e57f52cc6ea6b9e8
Aiyappa Ganesh <aiyappa@b-eagles.com>
2014-09-19 16:54:32 +0530

    Updating flow and also adding add member page

--- src/handlers/web/index.py
+++ src/handlers/web/index.py
@@ -19,10 +19,17 @@ class StartupRegistrationPage(WebRequestHandler):
         template_values = {}
         self.write(self.get_rendered_html(path, template_values), 200)
 
+class AddMemberPage(WebRequestHandler):
+    def get(self):
+        path = 'add_member.html'
+        template_values = {}
+        self.write(self.get_rendered_html(path, template_values), 200)
+
 app = webapp2.WSGIApplication(
     [
         ('/', IndexPage),
         ('/startup_login', StartupLoginPage),
-        ('/startup_registration', StartupRegistrationPage)
+        ('/startup_registration', StartupRegistrationPage),
+        ('/add_member', AddMemberPage)
     ]
 )
