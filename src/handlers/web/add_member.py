7477b84cb5036a3a8e68111afc23370796103cf7
Ranju <ranju@b-eagles.com>
2014-09-19 15:08:01 +0530

    Added a new expose_third_party endpoint

--- src/handlers/web/add_member.py
+++ src/handlers/web/add_member.py
@@ -6,13 +6,19 @@ class AddMemberPage(WebRequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('Hello, ' + user.nickname())
+            self.redirect('/member/expose_third_party')
         else:
-            self.redirect(users.create_login_url(self.request.uri))
+            self.redirect(users.create_login_url('/member/expose_third_party'))
+
+class ExposeThirdPartyPage(WebRequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, ' + user.nickname())
 
 app = webapp2.WSGIApplication(
     [
-        ('/add_member', AddMemberPage)
+        ('/member/add', AddMemberPage),
+        ('/member/expose_third_party', ExposeThirdPartyPage)
     ]
 )
