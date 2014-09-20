7b9ed6b1de6b1f473527e47830780f6f74a14dca
Ashray <ashray.velapanur@gmail.com>
2014-09-20 18:56:11 +0530

    pulling linkedin user data

--- src/user_data/linkedin.py
+++ src/user_data/linkedin.py
@@ -7,10 +7,15 @@ from google.appengine.ext import deferred
 
 import linkedin_config as linkedin
 
-def pull_profile(user, third_party_user):
-    response = json.loads(urlfetch.fetch(linkedin.PROFILE_URL%('num-connections,skills', third_party_user.access_token)).content)
-    connections = response['numConnections']
-    skills = [skill['skill']['name'] for skill in response['skills']['values']]
+    '''
+    skills = [skill['skill']['name'] for skill in response['skills']['values']] if response['skills']['_total'] > 0 else None
+    companies = [position['company']['name'] for position in response['positions']['values']] if response['positions']['_total'] > 0 else None
+    schools = [education['educations']['name'] for education in response['educations']['values']] if response['educations']['_total'] > 0 else None
+    '''
 
 def pull_data(user, third_party_user):
-    deferred.defer(pull_profile, user, third_party_user)
+    response = json.loads(urlfetch.fetch(linkedin.PROFILE_URL%('num-connections,num-recommenders,publications,patents,skills,positions,educations,certifications', third_party_user.access_token)).content)
+    connections = response['numConnections']
+    recommenders = response['numRecommenders']
+    patents = response['patents']['_total'] if 'patents' in response else None
+    publications = response['publications']['_total'] if 'publications' in response else None
