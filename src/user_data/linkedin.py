75f9051369c14a4db9cb795ec19e6b3b953738f4
Ashray <ashray.velapanur@gmail.com>
2014-09-20 19:11:35 +0530

    influence score calucations

--- src/user_data/linkedin.py
+++ src/user_data/linkedin.py
@@ -1,17 +1,12 @@
 import json
 import logging
 import urllib2
+import math
 
 from google.appengine.api import urlfetch
 from google.appengine.ext import deferred
 
 import linkedin_config as linkedin
-
-    '''
-    skills = [skill['skill']['name'] for skill in response['skills']['values']] if response['skills']['_total'] > 0 else None
-    companies = [position['company']['name'] for position in response['positions']['values']] if response['positions']['_total'] > 0 else None
-    schools = [education['educations']['name'] for education in response['educations']['values']] if response['educations']['_total'] > 0 else None
-    '''
 
 def pull_data(user, third_party_user):
     response = json.loads(urlfetch.fetch(linkedin.PROFILE_URL%('num-connections,num-recommenders,publications,patents,skills,positions,educations,certifications', third_party_user.access_token)).content)
@@ -19,3 +14,8 @@ def pull_data(user, third_party_user):
     recommenders = response['numRecommenders']
     patents = response['patents']['_total'] if 'patents' in response else None
     publications = response['publications']['_total'] if 'publications' in response else None
+
+    influence_raw = (4 * connections) + (4 * recommenders) + (1 * patents) + (1 * publications)
+    influence = math.log(influence_raw)/10.0
+    user.influence = influence if influence < 1.0 else 1.0
+    user.put()
